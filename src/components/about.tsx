1b756da269ab7a4fddb90f6ed708c32cf5f6a4de
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2025-03-09 20:37:19 -0300

    feat(homepage): add technology information to the about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,24 +1,20 @@
-import javascriptImage from "../assets/javascript.svg";
-
-const technologies: string[] = [
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
-  "React",
-  "TypeScript",
-  "Tailwind CSS",
+const technologies: { name: string; image: string }[] = [
+  { name: "Javascript", image: "/public/javascript.svg" },
+  { name: "Typescript", image: "/public/typescript.svg" },
+  { name: "Tailind", image: "/public/tailwind.svg" },
+  { name: "Styled Components", image: "/public/styled-components.svg" },
+  { name: "Mongoose", image: "/public/mongoose.svg" },
+  { name: "Knex", image: "/public/knex.svg" },
+  { name: "NodeJS", image: "/public/nodejs.svg" },
+  { name: "React", image: "/public/react.svg" },
+  { name: "NextJS", image: "/public/nextjs.svg" },
+  { name: "NestJS", image: "/public/nestjs.svg" },
+  { name: "Express", image: "/public/express.svg" },
+  { name: "TypeORM", image: "/public/typeorm.svg" },
+  { name: "Terraform", image: "/public/terraform.svg" },
+  { name: "Docker", image: "/public/docker.svg" },
+  { name: "Mysql", image: "/public/mysql.svg" },
+  { name: "Linux", image: "/public/linux.svg" },
 ];
 
 const About: React.FC = () => {
@@ -28,29 +24,52 @@ const About: React.FC = () => {
         SOBRE MIN
       </h2>
       <div className="flex justify-center">
-        <p className="text-center w-10/12">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+        <p className="w-8/12">
+          Desde muito cedo meu interesse por computadores vinha a tona, sempre
+          busquei conhecimento nesta área com bastante entusiasmo mesmo não
+          tendo computador e apenas um celular.
+          <br />
+          <br />
+          De início eu era focado em adquirir conhecimento na área do Hacking
+          (que me proporcionou uma base de criptografia, redes,
+          vulnerabilidades, protocolos TCP entre outros) e no uso de sistema
+          Linux, após alguns anos decidi migrar para a área da programação,
+          iniciei com Python mais me desenvolvi de verdade com a linguajem
+          Javascript como Full Stack.
+          <br />
+          <br />
+          De repente surgiu uma oportunidade de trabalhar com design gráfico, o
+          que me proporcionou experiencia nesta área.
+          <br />
+          <br />
+          Ando constantemente buscando conhecimento com a meta de me aperfeiçoar
+          e poder me tornar um bom profissional, Tenho facilidade de aprender e
+          tenho alegria ao poder ensinar ou ajudar.
+          <br />
+          <br />
+          Se tiver interesse em me conhecer melhor pode entrar em contato, será
+          um prazer conhecê-lo(a).
         </p>
       </div>
 
-      <div className="flex flex-wrap justify-center mt-8">
-        {technologies.map((tech, index) => (
-          <span
-            key={index}
-            className="bg-emerald-200 text-emerald-800 font-semibold py-2 px-4 m-2 rounded-full"
-          >
-            <img
-              src={javascriptImage}
-              alt="JavaScript Logo"
-              className="inline-block w-6 h-6 mr-2"
-            />
-            {tech}
-          </span>
-        ))}
+      <div className="flex justify-center">
+        <div className="flex flex-wrap justify-center mt-8 w-10/12">
+          {technologies.map((tech, index) => (
+            <span
+              key={index}
+              className="bg-emerald-200 text-emerald-800 font-semibold py-2 px-4 m-2 rounded-full"
+            >
+              <img
+                src={tech.image}
+                alt="JavaScript Logo"
+                className="inline-block w-6 h-6 mr-2"
+              />
+              {tech.name}
+            </span>
+          ))}
+        </div>
       </div>
+      <div className="h-40"></div>
     </>
   );
 };

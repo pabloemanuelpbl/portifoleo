c7eeed211958e90a4ef6db1c378c987d312b72e9
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2025-03-08 22:43:54 -0300

    feat(homepage): add the first version of the Footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4 text-center">
+            <p>© 2025 Pablo Emanuel</p>
+        </footer>
+    );
+  };
+  
+  export default Footer;

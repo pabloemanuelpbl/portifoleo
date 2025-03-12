49aa2fa5d172c1b487a795a89f4308d48b49ae61
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2025-03-12 15:42:33 -0300

    feat(homepage): change project descriptions

--- src/user/projects.ts
+++ src/user/projects.ts
@@ -9,25 +9,26 @@ import { ProjectsPropsInterface } from "../components/projects";
 const myProjects: ProjectsPropsInterface = {
   projects: [
     {
-      title: "Clone-Netflix",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec libero.",
+      title: "Clone Netflix",
+      description: 
+        "Um projeto simples que recria a interface de um site de vídeos, utilizando HTML e CSS.",
       image: cloneNetflixPng,
       github: "https://github.com/pabloemanuelpbl/Clone-Netflix",
       link: "https://pabloemanuelpbl.github.io/Clone-Netflix/",
     },
     {
-      title: "login-page",
+      title: "Login Page",
       description:
-        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec libero.".repeat(
-          6
-        ),
+        "Uma página de login com a possibilidade de alternar entre a área de login, a área de recuperação de senha, de cadastro de usurário e mudança de senha." +
+        "Foi usado Typescript, React + Vite e Tailwind. Para adicionar a navegação entre páginas, foi usada a biblioteca react-router-dom." + 
+        "Também foi utilizado o pattern de decomposição para que os componentes pudessem ser reutilizados em todas as páginas.",
       image: loginPagePng,
       github: "https://github.com/pabloemanuelpbl/login-page",
     },
     {
-      title: "jogo_space_shooter",
-      description: "Description for project 3",
+      title: "Space Shooter",
+      description: 
+        "Um jogo simples no front end onde você controla um alienígena no espaço e tenta se proteger dos objetos que passam pela tela.",
       image: spaceGamePng,
       github: "https://github.com/pabloemanuelpbl/jogo_space_shooter",
       link: "https://pabloemanuelpbl.github.io/jogo_space_shooter/",
@@ -35,22 +36,22 @@ const myProjects: ProjectsPropsInterface = {
     {
       title: "Compasscar",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec libero.",
+        "Este é um projeto que construí no estágio que realizei na Compass UOL, ele é uma API de gerenciamento de carros que segue o padrão de arquitetura REST." + 
+        " Este projeto foi construído utilizando o framework Nestjs, Knex para trabalhar com banco MySQL, ESLint para padronizar o código e Jest para implementação de testes E2E na aplicação.",
       image: compasscarPng,
       github: "https://github.com/pabloemanuelpbl/ANOUT24_D01_COMPASSCAR"
     },
     {
       title: "MySQL",
       description:
-        "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut purus eget nunc. Donec nec nunc nec libero.".repeat(
-          6
-        ),
+        "Aqui temos uma bateria de arquivos com exemplos de uso de querys SQL.",
       image: mysqlPng,
       github: "https://github.com/pabloemanuelpbl/AWS_NODE_OUT24_SEMANA_01"
     },
     {
       title: "Infraestrutura na AWS com Terraform",
-      description: "Description for project 3",
+      description: 
+        "Este é um projeto que construí no estágio que realizei na Compass UOL, a intenção foi de poder fazer o deploy de uma aplicação construída em grupo, nele contem descrições de como realiza o processo manual, mas também tem os arquivos do Terraform para a automação da criação da infraestrutura.",
       image: terraformAWSPng,
       github: "https://github.com/pabloemanuelpbl/ANOUT_OUT24_D03_AWS"
     },
